4eaa46e6cb41628b5160ff2c4a09ba3e0cfccb64
MarcosSantos223 <99343466+MarcosSantos223@users.noreply.github.com>
2022-02-23 12:41:20 -0300

    Add files via upload

---
+++ exercicio_01/main (1).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float e_p1, e_p2, e_L, s_media;
+    scanf("%float",&e_p1);
+    scanf("%float",&e_p2);
+    scanf("%float",&e_L);
+    s_media=e_p1+e_p2+e_L;
+    printf("%f\n",s_media);
+    
+    
+       return 0;
+}
