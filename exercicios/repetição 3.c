519621f4ab8f1b2d6ee6431b3cba157b42278ea6
MarcosSantos223 <99343466+MarcosSantos223@users.noreply.github.com>
2022-03-16 12:44:44 -0300

    Add files via upload

---
+++ exercicios/repetição 3.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    // declaracao de variavel
+    int e_numero , valor_aux ,s_fatorial;
+    printf("entre com o e_numero:\n");
+    scanf("%d",&valor_aux * s_fatorial);
+    =0;
+       while(s_fatorial<=e_numero{
+        printf ("%d \n",valor_aux );
+        valor_aux++;
+    }
+
+    return 0;
+}
