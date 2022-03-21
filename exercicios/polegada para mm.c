78864c3dbd8fbb059bde347c4495dab647e711a8
MarcosSantos223 <99343466+MarcosSantos223@users.noreply.github.com>
2022-03-20 21:02:10 -0300

    Add files via upload

---
+++ exercicios/polegada para mm.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+  #define e_25_4 25.4 
+    float e_polegada, s_mm;
+    scanf("%f",&e_polegada);
+    s_mm=e_25_4*e_polegada;
+    printf("%f\n",s_mm);
+    
+    
+       return 0;
+}
