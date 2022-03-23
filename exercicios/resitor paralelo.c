9baedf2d39267a5b1d6fd9cbd542d350b97ffc97
MarcosSantos223 <99343466+MarcosSantos223@users.noreply.github.com>
2022-03-23 11:56:43 -0300

    Add files via upload

---
+++ exercicios/resitor paralelo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float e_r1, e_r2, e_r3, e_r4 , s_sp , s_req;
+    printf ("entre com o resistor 1:");
+    scanf("%f",&e_r1);
+    printf ("entre com o resistor 2:");
+    scanf("%f",&e_r2);
+    printf ("entre com o resistor 3:");
+    scanf("%f",&e_r3);
+    printf ("entre com o resistor 4:");
+    scanf("%f",&e_r4);
+    s_sp=e_r1,+1/e_r2,+1/e_r3,+1/e_r4;
+    s_req=1/s_sp;
+    printf("resistor equivalente %f\n", s_req);
+    
+    
+       return 0;
+}
