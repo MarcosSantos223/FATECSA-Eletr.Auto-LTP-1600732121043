13f1f1742107549ec2ddd3d4eba8cfd3fa6a9bec
MarcosSantos223 <99343466+MarcosSantos223@users.noreply.github.com>
2022-06-12 03:43:22 -0300

    Update multiplicar while.c

--- exercicios/multiplicar while.c
+++ exercicios/multiplicar while.c
@@ -3,21 +3,21 @@
 int main()
 
 {
-    int v_aux, e_multiplicador, e_multiplicando , s_resultado ;
+    int v_aux, e_multiplicador, e_multiplicando , s_soma;
 
     printf("Entre com o valor do multiplicando:\n");
     scanf("%i",&e_multiplicando);
     printf("Entre com o valor do multiplicador:\n");
     scanf("%i",&e_multiplicador);
     v_aux=0;
-    s_resultado=0;
+    s_soma=0;
     
     while (v_aux<e_multiplicador){
-        s_resultado=e_multiplicando+e_multiplicando;
+        s_soma=s_soma+e_multiplicando;
         v_aux++;
         
         }
-        printf("O resultado eh:\n %i",s_resultado);
+        printf("Resultado :\n %i",s_soma);
     
     return 0;
 }
